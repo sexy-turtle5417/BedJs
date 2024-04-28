c44f4318f7c5c14173652f602632a2a65a4c8a01
Jaypee P. Zulieta <jaypee.zulieta@lsu.edu.ph>
2024-04-28 11:38:50 +0800

    fix: fix imports to import bed response instead of bed response builder

--- lib/routes/put.route.ts
+++ lib/routes/put.route.ts
@@ -1,5 +1,5 @@
 import BedRequest from "../request/bed-request";
-import BedResponse from "../response/bed-response.builder";
+import BedResponse from "../response/bed-response";
 import Route from "./route";
 
 interface PUTRoute extends Route {
